0de916d3bb26ce7e44db04e2d1bcceb435633350
aliosmankaya <aliosman.kaya@b2metric.com>
2022-01-09 16:41:33 +0300

    edited template and added css

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from predict import predict, columns, model
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='TEMPLATES',
+    template_folder="TEMPLATES"
+    )
+
+app.secret_key = "\x84/!\x16&\xe6\xeb\x8an\n\na\re\xb3\x11\xe5\xf1\xf6\xdd\xf8\xed\x05G"
 
 
 @app.route("/", methods=["GET"])
@@ -9,9 +17,20 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET","POST"])
 def prediction():
-    pass
+
+    if request.method == "POST":
+
+        checks = request.form.getlist("options")
+        age = checks[0]
+        pred = predict(columns, age, checks, model)       
+
+        return render_template("index.html", pred=pred)
+
+    else:
+        return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- predict.py
+++ predict.py
@@ -1,5 +1,45 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 from joblib import load
 
 # Load model
 model = load("model.joblib")
 
+# Data columns
+columns = [
+    'gender', 'polyuria', 'polydipsia', 'sudden_weight_loss',
+    'weakness', 'polyphagia', 'genital_thrush', 'visual_blurring',
+    'itching', 'irritability', 'delayed_healing', 'partial_paresis',
+    'muscle_stiffness', 'alopecia', 'obesity',
+]
+
+# Predict function
+def predict(columns, age, checks, model):
+
+    checkList = []
+    data = pd.read_csv("diabetes_data.csv", sep=";")
+
+    scaler = MinMaxScaler()
+    scaler.fit_transform(data.age.values.reshape(-1,1))
+    age = scaler.transform(np.array([age]).reshape(-1,1))
+    checkList.append(age[0][0])
+
+    for i in columns:
+
+        if i in checks:
+            checkList.append(1)
+
+        else:
+            checkList.append(0)
+
+    pred = model.predict(np.array([checkList,]))
+    pred_proba = model.predict_proba(np.array([checkList,]))
+
+    if pred[0] == 0:
+        pred_text = f"You're ok\nAccuracy: {pred_proba[0][pred[0]]}"
+
+    elif pred[0] == 1:
+        pred_text = f"You have diabetes\nAccuracy: {pred_proba[0][pred[0]]}"
+
+    return pred_text
